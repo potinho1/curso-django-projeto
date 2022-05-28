d5726051cd630469c9d7a573eadf919b4bc2f9ef
João Vitor Gomes Da Silva <gomesjoaovitor222@gmail.com>
2022-05-28 11:06:08 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,13 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'POTINHO',
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html', context={
-        'name1': 'pots',
-    })
-
-
-def sobre(request):
-    return HttpResponse('sobre')
