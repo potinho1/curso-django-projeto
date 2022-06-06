bea0053fe619829e82b4461823c93fd562cf0daa
João Vitor Gomes Da Silva <gomesjoaovitor222@gmail.com>
2022-06-06 20:16:54 -0300

    Added recipe-view template

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'POTINHO',
     })
+
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'POTINHO',
+    })
