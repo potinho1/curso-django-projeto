fb83bbb04bcbb11a7ce7f1cf22db94c6ebdb9ee0
João Vitor Gomes Da Silva <gomesjoaovitor222@gmail.com>
2022-06-09 20:43:15 -0300

    Trabalhando nas diferenças entre detail page e list page

--- recipes/views.py
+++ recipes/views.py
@@ -4,11 +4,12 @@ from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': [make_recipe() for _ in range(1)],
     })
 
 
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
